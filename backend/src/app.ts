5be2983a5f943fdf543fa5272a44b08619a20939
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-04-15 16:13:02 -0500

    MongoDB model setup
    MongoDB model setup + Express error handling (+ what  is middleware)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,33 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import NoteModel from "./models/note";
+
 
 const app = express();
 
-app.get("/",(req, res)=>{
-    res.send("Hello, world!");
+app.get("/",async (req, res, next)=>{
+    try {
+        // throw Error("Bazzinga!!");
+        const notes = await NoteModel.find().exec();
+        res.status(200).json(notes);
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+app.use((req, res, next)=>{
+    next(Error("Endpoint not Found!!"));
+});
+
+
+app.use((error:unknown, req:Request, res:Response, next:NextFunction)=>{
+    console.log(error);
+    let errorMessage="An Unknown Error Occurred!";
+    if(error instanceof Error){
+        errorMessage=error.message;
+    }
+    res.status(500).json({error: errorMessage});
 });
 
 export default app;
